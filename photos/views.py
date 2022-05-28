2072fee715d260d20c589353ba26b9fcef0c32f8
Mercynenoh <mercychepngenoh74@gmail.com>
2022-05-29 02:53:08 +0300

    add photo template

--- photos/views.py
+++ photos/views.py
@@ -11,7 +11,8 @@ def gallery(request):
     return render(request, 'photos/gallery.html', context)
 
 def viewPhoto(request, pk):
-    return render(request, 'photos/photo.html')
+    photo = Photo.objects.get(id=pk)
+    return render(request, 'photos/photo.html', {'photo':photo})
 
 def newPhoto(request):
     return render(request, 'photos/new.html')

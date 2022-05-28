087528ecd1cd9d3f57f2d8e3ffb80936962a732a
Mercynenoh <mercychepngenoh74@gmail.com>
2022-05-29 01:54:11 +0300

    initial commit

---
+++ gallery/photos/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+# from .models import Category, Photo, Author, Location
+
+# # Register your models here.
+# class ArticleAdmin(admin.ModelAdmin):
+#     list_display = ['category']
+#     ordering = ['category']
+   
+
+# admin.site.register(Photo)
+# admin.site.register(Author)
+# admin.site.register(Category, ArticleAdmin)
+# admin.site.register(Location)
---
+++ gallery/photos/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Author(models.Model):
+    first_name = models.CharField(max_length =30,null='False', blank='False')
+    last_name = models.CharField(max_length =30,null='False', blank='False')
+    email = models.EmailField(null='False', blank='False')
+    phone_number = models.CharField(max_length = 10,blank =True)
+
+    def __str__(self):
+        return self.last_name
+    class Meta:
+        ordering = ['last_name']  
+        
+class Category(models.Model):
+    category = models.CharField(max_length =30, null='False', blank='False')
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    
+    def __str__(self):
+        return self.category
+class Location(models.Model):
+    country =  models.CharField(max_length =10)
+    city =  models.CharField(max_length =10)
+    
+    def __str__(self):
+        return self.country, self.city
+
+class Photo(models.Model):
+    image = models.ImageField(null='False', blank='False',upload_to = 'images/',default='IMAGE')
+    imagename =  models.CharField(max_length =30)
+    description = models.TextField(null='False', blank='False')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.description
+
+

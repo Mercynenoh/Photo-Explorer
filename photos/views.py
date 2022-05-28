03c1c39343c7c1ac0faaa120c22e73cfbab03f07
Mercynenoh <mercychepngenoh74@gmail.com>
2022-05-29 02:29:04 +0300

    Create templates

--- photos/views.py
+++ photos/views.py
@@ -3,7 +3,12 @@ from .models import Author, Category, Location, Photo
 
 # Create your views here.
 def gallery(request):
-    return render(request, 'photos/gallery.html')
+    categories = Category.objects.all()
+    authors = Author.objects.all()
+    locations = Location.objects.all()
+    photos = Photo.objects.all()
+    context = {'categories': categories, 'authors': authors, 'locations':locations, 'photos':photos}
+    return render(request, 'photos/gallery.html', context)
 
 def viewPhoto(request, pk):
     return render(request, 'photos/photo.html')
